b27908c2c282c778d205b8e94a0f7cbe3292f625
hyunjin9603 <hyunjin9603@gmail.com>
2023-02-27 17:43:58 +0900

    feat:s3

--- routes/communities.route.js
+++ routes/communities.route.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const auth = require('../middlewares/authMiddleware');
 const CommunityController = require('../controllers/communities.controller');
 const communityController = new CommunityController();
+const Upload = require('../middlewares/ImgUploadMiddleware');
+const upload = new Upload();
 
-router.post('/', auth, communityController.createCommunity);
+router.post('/', auth,upload.upload.single('image'), communityController.createCommunity);
 router.get('/',auth, communityController.findAllCommunity)
 router.get('/:communityId',auth, communityController.findCommunity)
-router.put('/:communityId',auth,communityController.updateCommunity)
-router.delete('/:communityId',auth,communityController.deleteCommunity)
+router.put('/:communityId',auth,upload.upload.single('image'),communityController.updateCommunity)
+router.delete('/:communityId',auth,upload.delete_file,communityController.deleteCommunity)
 
 module.exports = router;
